22a4bb4949a997eb3773cd8c080de67b9fc6dff5
Khalid-assalmi <assalmi85wrk@gmail.com>
2026-02-15 19:51:27 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,72 @@
+let settingsBtn = document.getElementById("settingsBtn");
+let searchInp = document.getElementById("searchInput");
+let searchBtn = document.getElementById("searchButton");
+let div = document.createElement("div");
+div.className = "settingsBox";
+let settingCard = document.createElement("div");
+settingCard.className = "settingCard";
+let conditions = document.createElement("a");
+conditions.textContent = "الشروط والأحكام";
+let conditionsIcon = document.createElement("i");
+conditionsIcon.className = "fa-solid fa-circle-info";
+let settingCard2 = document.createElement("div");
+settingCard2.className = "settingCard";
+let payment = document.createElement("a");
+payment.textContent = "الدفع و الاستلام"
+settingCard2.id = "paymentCard";
+let paymentIcon = document.createElement("i");
+paymentIcon.className = "fa-regular fa-credit-card";
+let settingCard3 = document.createElement("div");
+settingCard3.className = "settingCard";
+let socailMedia = document.createElement("a");
+socailMedia.textContent = "وسائل التواصل الاجتماعي";
+let socailMediaIcon = document.createElement("i");
+socailMediaIcon.className = "fa-brands fa-whatsapp";
+let turn = false;
+settingsBtn.addEventListener("click", (e) => {
+    if (!turn) {
+        settingsBtn.appendChild(div);
+        div.append(settingCard, settingCard2, settingCard3);
+        settingCard.append(conditions, conditionsIcon);
+        settingCard2.append(payment, paymentIcon);
+        settingCard3.append(socailMedia, socailMediaIcon);
+        e.stopPropagation();
+        turn = true;
+    } else {
+        turn = false;
+    }
+});
+div.addEventListener("click", (e) => {
+    e.stopPropagation();
+});
+document.addEventListener("click", () => {
+    div.style.animationName = "hideSettingsBox";
+    setTimeout(() => {
+        div.style.animationName = "";
+        div.remove();
+    }, 40);
+    turn = false;
+});
+searchInp.addEventListener("focus", () =>  {
+    searchBtn.style.display = "block";
+    searchInp.style.maxWidth = "550px";
+    searchInp.style.width = "60vw";
+    searchInp.style.padding = "0 12px 0 24px";
+    searchInp.style.borderRadius = "12px";
+    searchInp.style.transition = "1.2s";
+    setInterval(() => {
+        if (searchInp.value) {
+            searchBtn.style.removeProperty("--main");
+        } else {
+            searchBtn.style.setProperty("--main", "#3b3b3b");
+        }
+    });
+});
+searchInp.addEventListener("blur", () =>  {
+    searchBtn.style.display = "";
+    searchInp.style.maxWidth = "";
+    searchInp.style.width = "";
+    searchInp.style.padding = "";
+    searchInp.style.borderRadius = "";
+    searchInp.style.transition = "0.7s";
+});
